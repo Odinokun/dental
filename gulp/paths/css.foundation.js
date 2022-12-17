1c6f6deff1f61333db57aacbf2f7f5f861a723c2
odinokun <odinokun@gmail.com>
2022-12-17 13:05:56 +0100

    start project

---
+++ gulp/paths/css.foundation.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = [
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/swiper/swiper-bundle.css',
+  './src/vendors/twentytwenty-master/css/twentytwenty.css',
+];
---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  './node_modules/swiper/swiper-bundle.js',
+  './src/vendors/twentytwenty-master/js/jquery.event.move.js',
+  './src/vendors/twentytwenty-master/js/jquery.twentytwenty.js'
+
+
+];
