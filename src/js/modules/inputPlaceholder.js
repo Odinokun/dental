e570c1daf413a313c90476bd3803e4407bd5ff51
odinokun <odinokun@gmail.com>
2023-01-03 17:25:49 +0100

    did contacts page

--- src/js/modules/inputPlaceholder.js
+++ src/js/modules/inputPlaceholder.js
@@ -1,14 +1,21 @@
 module.exports = function () {
 
-  const targetInput = $('.form__input').find('input');
+  const targetInput = $('.input').find('input');
 
   $(targetInput).on('keyup', function () {
-    const targetSpan = $(this).next('span');
-
     if (this.value.length > 0) {
-      $(this).parent('.form__input').addClass('active');
+      $(this).parent('.input').addClass('active');
     } else {
-      $(this).parent('.form__input').removeClass('active')
+      $(this).parent('.input').removeClass('active')
+    }
+  })
+
+  const targetTextarea = $('.textarea').find('textarea');
+  $(targetTextarea).on('keyup', function () {
+    if (this.value.length > 0) {
+      $(this).parent('.textarea').addClass('active');
+    } else {
+      $(this).parent('.textarea').removeClass('active')
     }
   })
 
--- src/js/uncompressed.js
+++ src/js/uncompressed.js
@@ -7,6 +7,7 @@ $(document).ready(function ($) {
     'services',
     'faq',
     'reviews',
+    'contacts',
     ]);
 });
 
